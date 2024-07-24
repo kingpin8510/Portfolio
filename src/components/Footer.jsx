1a063b1d7dfcc1f81c1e0cc0f4bbd535e36eecfa
kingpin8510 <rp240479@gmail.com>
2024-07-24 12:52:09 +0530

    Updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="py-4 text-center bg-primary text-white "> Being what it takes !! </div>;
+  return <div className="py-4 text-center bg-primary text-white "> Building the Web, One Line at a Time !! </div>;
 };
 
 export default Footer;
